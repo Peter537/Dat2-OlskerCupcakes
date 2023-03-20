22a39cc3b71f10f00522df65f9d2af2ad1c70230
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-03-20 11:32:47 +0100

    Added methods to CupcakeMapper and Facade

--- src/main/java/dat/backend/model/persistence/CupcakeFacade.java
+++ src/main/java/dat/backend/model/persistence/CupcakeFacade.java
@@ -1,5 +1,32 @@
 package dat.backend.model.persistence;
 
+import dat.backend.model.entities.Bottom;
+import dat.backend.model.entities.ShoppingCart;
+import dat.backend.model.entities.Top;
+import dat.backend.model.entities.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class CupcakeFacade {
 
+    public static Top getTopById(int id, ConnectionPool connectionPool) {
+        return CupcakeMapper.getTopById(id, connectionPool);
+    }
+
+    public static Bottom getBottomById(int id, ConnectionPool connectionPool) {
+        return CupcakeMapper.getBottomById(id, connectionPool);
+    }
+
+    public static List<Top> getAllToppings(ConnectionPool connectionPool) {
+        return CupcakeMapper.getAllToppings(connectionPool);
+    }
+
+    public static List<Bottom> getAllBottoms(ConnectionPool connectionPool) {
+        return CupcakeMapper.getAllBottoms(connectionPool);
+    }
+
+    public static void createOrder(User user, LocalDateTime readyTime, ShoppingCart shoppingCart, ConnectionPool connectionPool) {
+        CupcakeMapper.createOrder(user, readyTime, shoppingCart, connectionPool);
+    }
 }
--- src/main/java/dat/backend/model/persistence/CupcakeMapper.java
+++ src/main/java/dat/backend/model/persistence/CupcakeMapper.java
@@ -1,5 +1,29 @@
 package dat.backend.model.persistence;
 
+import dat.backend.model.entities.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 class CupcakeMapper {
 
+    public static Top getTopById(int id, ConnectionPool connectionPool) {
+        return null;
+    }
+
+    public static Bottom getBottomById(int id, ConnectionPool connectionPool) {
+        return null;
+    }
+
+    public static List<Top> getAllToppings(ConnectionPool connectionPool) {
+        return null;
+    }
+
+    public static List<Bottom> getAllBottoms(ConnectionPool connectionPool) {
+        return null;
+    }
+
+    public static void createOrder(User user, LocalDateTime readyTime, ShoppingCart shoppingCart, ConnectionPool connectionPool) {
+
+    }
 }

db9d9c760d2831b76dc46ae41e59a173b790e18e
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-03-20 19:33:28 +0100

    Removed 2 methods moved to another facade

--- src/main/java/dat/backend/model/persistence/UserFacade.java
+++ src/main/java/dat/backend/model/persistence/UserFacade.java
@@ -1,6 +1,5 @@
 package dat.backend.model.persistence;
 
-import dat.backend.model.entities.Order;
 import dat.backend.model.entities.Role;
 import dat.backend.model.entities.User;
 import dat.backend.model.exceptions.DatabaseException;
@@ -18,16 +17,6 @@ public class UserFacade {
     }
 
     public static List<User> getAllUsers(ConnectionPool connectionPool) throws DatabaseException {
-        return null;
-        //return UserMapper.getAllUsers(connectionPool);
-    }
-
-    public static List<Order> getAllUserOrders(String email, ConnectionPool connectionPool) throws DatabaseException {
-        return null;
-        //return UserMapper.getAllUserOrders(email, connectionPool);
-    }
-
-    public static void deleteOrder(int id, ConnectionPool connectionPool) throws DatabaseException {
-        //UserMapper.deleteOrder(id, connectionPool);
+        return UserMapper.getAllUsers(connectionPool);
     }
 }

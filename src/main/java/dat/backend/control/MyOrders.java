747a4b2a7fe5d50d4a27332382e749e7eb45d219
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2023-03-21 13:34:54 +0100

    MyOrders servlet that displays users orders (history)

--- src/main/java/dat/backend/control/MyOrders.java
+++ src/main/java/dat/backend/control/MyOrders.java
@@ -1,9 +1,21 @@
 package dat.backend.control;
 
+import dat.backend.model.config.ApplicationStart;
+import dat.backend.model.entities.Order;
+import dat.backend.model.entities.User;
+import dat.backend.model.exceptions.DatabaseException;
+import dat.backend.model.persistence.OrderFacade;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static dat.backend.model.persistence.OrderFacade.getAllOrdersByUser;
 
 @WebServlet(name = "MyOrders", value = "/MyOrders")
 public class MyOrders extends HttpServlet {
@@ -14,6 +26,26 @@ public class MyOrders extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+
+            Connection connection = ApplicationStart.getConnectionPool().getConnection();
+
+            User user = (User) request.getSession().getAttribute("user");
+           List<Order> orderList =  OrderFacade.getAllOrdersByUser(user, connection);
+
+           for(Order order : orderList){
+
+               System.out.println(order);
+
+           }
+            request.setAttribute("orderList", orderList);
+            request.getRequestDispatcher("WEB-INF/myOrders.jsp").forward(request, response);
+
+        } catch (DatabaseException | SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }

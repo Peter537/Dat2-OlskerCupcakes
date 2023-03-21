d596d84eb1bad3a2e57eb73133589a268e6364d6
Yusuf Al-Khafaji <GLBcraft1234@gmail.com>
2023-03-21 11:14:27 +0100

    Temporary push

--- src/main/java/dat/backend/model/persistence/OrderMapper.java
+++ src/main/java/dat/backend/model/persistence/OrderMapper.java
@@ -1,9 +1,6 @@
 package dat.backend.model.persistence;
 
-import dat.backend.model.entities.Cupcake;
-import dat.backend.model.entities.Order;
-import dat.backend.model.entities.ShoppingCart;
-import dat.backend.model.entities.User;
+import dat.backend.model.entities.*;
 import dat.backend.model.exceptions.DatabaseException;
 
 import java.sql.*;
@@ -49,14 +46,11 @@ class OrderMapper {
 
             while (rs.next()) {
                 int order_id = rs.getInt("order_id");
-                String email = rs.getString("fk_user_email");
                 LocalDateTime time = rs.getDate("readytime").toLocalDate().atStartOfDay();
-                float totalprice = rs.getFloat("totalprice");
-                int cupcakecount = rs.getInt("cupcakecount");
-                String status = rs.getString("status");
+                OrderStatus status = OrderStatus.valueOf(rs.getString("status").toUpperCase());
                 ShoppingCart shoppingCart = getShoppingCartByOrderId(order_id, connection);
-                Order order = new Order(order_id, user, time, shoppingCart);
-                //TODO: Figure out how shoppingCart plays into the above line
+                Order order = new Order(order_id, user, time, shoppingCart, status);
+
             }
 
         } catch (SQLException e) {
@@ -117,6 +111,28 @@ class OrderMapper {
 
     private static ShoppingCart getShoppingCartByOrderId(int orderId, Connection connection) {
         ShoppingCart shoppingCart = new ShoppingCart();
+        ArrayList<Cupcake> cupcakeList = new ArrayList<>();
+        String sql = "SELECT * FROM cupcake WHERE fk_order_id = ?";
+
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, orderId);
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                int cupcake_id = rs.getInt("cupcake_id");
+                int fk_cupcaketop_id = rs.getInt("fk_cupcaketop_id");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
 
         // TODO: Implement this method
 

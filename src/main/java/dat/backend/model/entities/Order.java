e08addddfb3b10cee5dd24fc974e283467531ef3
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-03-21 13:34:39 +0100

    Removed ShoppingCart from Order

--- src/main/java/dat/backend/model/entities/Order.java
+++ src/main/java/dat/backend/model/entities/Order.java
@@ -7,29 +7,25 @@ public class Order {
     private int id;
     private User user;
     private LocalDateTime readyTime;
-    private ShoppingCart shoppingCart;
     private OrderStatus status;
 
-    public Order(User user, LocalDateTime readyTime, ShoppingCart shoppingCart) {
+    public Order(User user, LocalDateTime readyTime) {
         this.user = user;
         this.readyTime = readyTime;
-        this.shoppingCart = shoppingCart;
         this.status = OrderStatus.IN_SHOPPING_CART;
     }
 
-    public Order(int id, User user, LocalDateTime readyTime, ShoppingCart shoppingCart) {
+    public Order(int id, User user, LocalDateTime readyTime) {
         this.id = id;
         this.user = user;
         this.readyTime = readyTime;
-        this.shoppingCart = shoppingCart;
         this.status = OrderStatus.IN_SHOPPING_CART;
     }
 
-    public Order(int id, User user, LocalDateTime readyTime, ShoppingCart shoppingCart, OrderStatus status) {
+    public Order(int id, User user, LocalDateTime readyTime, OrderStatus status) {
         this.id = id;
         this.user = user;
         this.readyTime = readyTime;
-        this.shoppingCart = shoppingCart;
         this.status = status;
     }
 
@@ -57,14 +53,6 @@ public class Order {
         this.readyTime = readyTime;
     }
 
-    public ShoppingCart getShoppingCart() {
-        return this.shoppingCart;
-    }
-
-    public void setShoppingCart(ShoppingCart shoppingCart) {
-        this.shoppingCart = shoppingCart;
-    }
-
     public OrderStatus getStatus() {
         return this.status;
     }
@@ -79,7 +67,6 @@ public class Order {
                 "id=" + this.id +
                 ", user=" + this.user +
                 ", readyTime=" + this.readyTime +
-                ", shoppingCart=" + this.shoppingCart +
                 ", status=" + this.status +
                 '}';
     }

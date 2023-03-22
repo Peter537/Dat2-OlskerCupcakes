d7ee8d2c9c7b3d47d0d7b11a352f990b4d6261dd
TheLonelyDude <magnus.kongstad@gmail.com>
2023-03-22 14:09:13 +0100

    Now redirects correctly

--- src/main/java/dat/backend/control/ToUserpage.java
+++ src/main/java/dat/backend/control/ToUserpage.java
@@ -1,5 +1,8 @@
 package dat.backend.control;
 
+import dat.backend.model.entities.Role;
+import dat.backend.model.entities.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -10,6 +13,10 @@ public class ToUserpage extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/userpage.jsp").forward(request, response);
+        User user = (User) request.getSession().getAttribute("user");
+        if (user.getRole().equals(Role.ADMIN))
+            request.getRequestDispatcher("WEB-INF/adminpage.jsp").forward(request, response);
+        else
+            request.getRequestDispatcher("WEB-INF/userpage.jsp").forward(request, response);
     }
 }

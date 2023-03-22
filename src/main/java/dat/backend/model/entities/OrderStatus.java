228709836c5d8cca4a53deb0027c647a2170fd0c
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-03-22 14:48:48 +0100

    Added values to OrderStatus to easily sort them

--- src/main/java/dat/backend/model/entities/OrderStatus.java
+++ src/main/java/dat/backend/model/entities/OrderStatus.java
@@ -1,9 +1,19 @@
 package dat.backend.model.entities;
 
 public enum OrderStatus {
-    CANCELLED,
-    IN_SHOPPING_CART,
-    PENDING,
-    READY,
-    DELIVERED
+    CANCELLED(1),
+    IN_SHOPPING_CART(2),
+    PENDING(4),
+    READY(5),
+    DELIVERED(3);
+
+    private final int value;
+
+    OrderStatus(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
 }

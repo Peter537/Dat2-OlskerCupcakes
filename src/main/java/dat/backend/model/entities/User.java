ddae67f5de5e9b370d4d9cf9cb8e64758716c4b0
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-04-14 13:49:23 +0200

    removed try catch

--- src/main/java/dat/backend/model/entities/User.java
+++ src/main/java/dat/backend/model/entities/User.java
@@ -86,14 +86,11 @@ public class User {
     }
 
     public OrderStatus getLastOrderStatus() {
-        try {
-            if (this.getOrders().size() == 0) {
-                return OrderStatus.NO_ORDERS;
-            }
-            return this.getOrders().get(this.getOrders().size() - 1).getStatus();
-        } catch (IndexOutOfBoundsException e) {
+        if (this.getOrders().size() == 0) {
             return OrderStatus.NO_ORDERS;
         }
+
+        return this.getOrders().get(this.getOrders().size() - 1).getStatus();
     }
 
     public String getFormattedBalance() {
